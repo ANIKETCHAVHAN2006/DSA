e142613b343838575c9a50177ce89e748281b684
Aniket Chavhan <aniketchavhan991@gmail.com>
2026-08-06 22:04:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (68.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> st ;
+        for(int i = 0 ; i < str.size() ; i++){
+            if(str[i] == '(' || str[i] == '{' || str[i] == '['){ //OPENING
+             st.push(str[i]) ;
+            }
+            else {  //CLOSING
+             if(st.size() == 0){
+                return false ;
+             }
+             if((st.top() == '(' && str[i] ==')') || (st.top() == '{' && str[i] == '}') || (st.top () == '[' && str[i] ==']')){
+                st.pop() ;
+             }
+             else{   //N0 MATCH
+             return false ;
+             }
+            }
+        }
+        return st.size() == 0 ;
+    }
+};
