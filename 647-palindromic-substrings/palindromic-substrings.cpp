770b0d60666d3cff120bd1971b7a9b07e18e62b5
Aniket Chavhan <aniketchavhan991@gmail.com>
2026-09-19 22:06:50 +0530

    Time: 563 ms (12.42%) | Memory: 8.5 MB (77.65%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalin(string &s, int i, int j){
+        while(i<=j){
+          if(s[i++] != s[j--]){
+            return false;
+          }
+        }
+        return true;
+    }
+    int countSubstrings(string s) {
+        int n = s.length();
+        int count = 0;
+        for(int i = 0; i < n ; i++){
+            for(int j = i; j<n;j++){
+                if(isPalin(s,i,j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
