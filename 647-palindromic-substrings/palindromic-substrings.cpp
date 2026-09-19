bac8771d6af6c53b5dd3e742c51df76f9695f42a
Aniket Chavhan <aniketchavhan991@gmail.com>
2026-09-19 22:14:51 +0530

    Time: 2 ms (89.96%) | Memory: 8.5 MB (91.34%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
-    bool isPalin(string &s, int i, int j){
-        while(i<=j){
-          if(s[i++] != s[j--]){
-            return false;
-          }
-        }
-        return true;
-    }
-    int countSubstrings(string s) {
-        int n = s.length();
+    // bool isPalin(string &s, int i, int j){
+    //     while(i<=j){
+    //       if(s[i++] != s[j--]){
+    //         return false;
+    //       }
+    //     }
+    //     return true;
+    // }
+    // int countSubstrings(string s) {
+    //     int n = s.length();
+    //     int count = 0;
+    //     for(int i = 0; i < n ; i++){
+    //         for(int j = i; j<n;j++){
+    //             if(isPalin(s,i,j)){
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+
+     int expand(string &s, int l, int r) {
         int count = 0;
-        for(int i = 0; i < n ; i++){
-            for(int j = i; j<n;j++){
-                if(isPalin(s,i,j)){
-                    count++;
-                }
-            }
+
+        while(l >= 0 && r < s.length() && s[l] == s[r]) {
+            count++;
+            l--;
+            r++;
         }
+
+        return count;
+    }
+
+    int countSubstrings(string s) {
+        int count = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            // Odd length palindrome
+            count += expand(s, i, i);
+
+            // Even length palindrome
+            count += expand(s, i, i + 1);
+        }
+
         return count;
     }
 };
